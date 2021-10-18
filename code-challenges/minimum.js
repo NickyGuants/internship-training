40d74618bd5b3aae07b17ed442e291ba44ac0b8d
NickyGuants <nicholasguantai528@gmail.com>
2021-10-18 12:54:17 +0300

    solved using math object

--- code-challenges/minimum.js
+++ code-challenges/minimum.js
@@ -3,12 +3,8 @@
 
 //using javascript array method
 function findMinimum(arr){
-    let minimum=filteredArr[0];
-    for (let i=0; i<arr.length; i++){
-        if(arr[i]<minimum && typeof arr[i]==='number'){
-            minimum=arr[i];
-        }
-    }
-    return minimum;
+    let integers=arr.filter(value => typeof value === 'number');
+    let minValue=Math.min(...integers)
+    return minValue;
 }
-console.log(findMinimum(['a',23, 'hello', 4, 100, 'j', 12]))
+console.log(findMinimum([33,'a',23, 'hello', 4, 100, 'j', 12]))
