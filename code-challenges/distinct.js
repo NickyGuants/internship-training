c535446547bfbe2f8d0b9ce9d9d1533779c4a7e2
NickyGuants <nicholasguantai528@gmail.com>
2021-10-19 12:01:24 +0300

    fix

--- code-challenges/distinct.js
+++ code-challenges/distinct.js
@@ -4,15 +4,16 @@
 //I start by creating a new array to hold the distinct values
 //I then loop through the original array and check whether each element exists in 
 //my new array. If the element does not exist, I push it to the new array if it exists 
-//we continue to the 
+//we continue to the next element.
+
 function distinct(arr){
     let newArr=[];
     for(let i=0; i<arr.length; i++){
-        if(newArr.includes(arr[i])==false){
+        if(newArr.includes(arr[i])===false){
             newArr.push(arr[i]);
         }
     }
-    return newArr
+    return newArr;
 }
-console.log(distinct([1, 3, 5, 3, 7, 3, 1, 1, 5]))
+console.log(distinct([1, 3, 5, 3, 7, 3, 1, 1, 5]));
 
