3f6b455317babc298d977f3413ad7f6f3af544ac
NickyGuants <nicholasguantai528@gmail.com>
2021-10-20 13:06:45 +0300

    render products

--- e-commerce/index.js
+++ e-commerce/index.js
@@ -1,3 +1,5 @@
+const bedEl=document.querySelector(".products");
+
 let slideIndex = 0;
 showSlides();
 
@@ -10,6 +12,21 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 10000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+//fUNCTIONALITY
+function renderProducts(){
+  products.forEach((product)=>{
+    bedEl.innerHTML += `
+    <div class="card">
+      <img src="${product.imgSrc}" alt="${product.name}">
+      <p>${product.name}</p>
+      <p>${product.description}</p>
+      <button>Add to Cart</button>
+    </div>  
+    `
+  });
+}
+renderProducts();
 
