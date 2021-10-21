58e07cba1bfccb237c8209f8afe3fb3539f7f6fa
NickyGuants <nicholasguantai528@gmail.com>
2021-10-21 16:36:21 +0300

    change products

--- e-commerce/products.js
+++ e-commerce/products.js
@@ -2,7 +2,7 @@ const products = [
     {
       id: 0,
       name: "Double-Size Bed",
-      price: 100,
+      price: 1,
       instock: 43,
       description:
       "At 4ft 6in wide, double beds are a popular choice with couples and for solo sleepers who like a little extra space to stretch out in at night.",
@@ -11,7 +11,7 @@ const products = [
     {
       id: 1,
       name: "King-sized Bed",
-      price: 300,
+      price: 3,
       instock: 100,
       description:
       "At 5ft wide, a king size bed provides the luxury of extra space to move around in. From statement styles to tech and space saving options.",
@@ -20,7 +20,7 @@ const products = [
     {
       id: 2,
       name: "Super King Bed",
-      price: 5000,
+      price: 5,
       instock: 50,
       description:
       "Measuring 6ft wide, super king beds are our largest size bed frame. From luxurious upholstered designs to complement your bedroom décor.",
